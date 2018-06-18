d6496d8f9d0cf5d27455c5ff5b91734915289716
Catherine Bronzo <cbronzo@mac.com>
2018-06-18 02:31:54 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -4,7 +4,7 @@ string =  "Hello there, and how are you?"
 def reverse_each_word(string)
   new_array = string.split( )
   reverse_array = []
-  new_array.each do |word|
+  new_array.collect do |word|
   reverse_array << word.reverse
  end
  reverse_array.join(" ")
