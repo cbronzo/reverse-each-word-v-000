86d39c17b564216890810b541d9899a002228d70
Catherine Bronzo <cbronzo@mac.com>
2018-06-18 02:30:47 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -11,6 +11,14 @@ def reverse_each_word(string)
 end
   
 
-
+def reverse_each_word(string)
+  new_array = string.split( )
+  reverse_array = []
+  new_array.each do |word|
+  reverse_array << word.reverse
+ end
+ reverse_array.join(" ")
+end
+  
 
   
